03ec36ca121c6fa85f23b2411176f13e5c09fa7e
Sami <sami@nieminen.fi>
2017-08-20 20:19:33 +0300

    updated index.html to have short description. Activate nav-items using react-router's redux state

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -3,20 +3,23 @@ import { Link } from 'react-router';
 import { connect } from 'react-redux';
 import { changeLanguage } from '../actions/contentActions';
 
-const Header = ({ content, languageCode, languages, changeLanguage }) => {
+// const Header = ({ content, languageCode, languages, changeLanguage, params }) => {
+const Header = ({ content, languageCode, languages, changeLanguage, pathname }) => {
 
   const { navItems } = content;
+  // Set nav item active when location matches item.location path
+  // react-router Link component should handle case automatically but doesn't work when using lazy initialization
   return (
     <div className="navbar-fsobp margin-clear">
       <div className="nav-container row middle-xs between-xs start-xs around-sm">
-        <div className="col-xs-6 col-sm-3" >
+        <div className="col-xs-12 col-sm-3 center-xs start-sm" >
           <ul className="list-inline">
             {navItems.map(item => (
-              <li key={item.text}><Link to={item.location} className="hvr-underline-reveal">{item.text}</Link></li>
+              <li key={item.text}><Link to={item.location} className={'hvr-underline-reveal ' + (pathname === item.location && 'active')}>{item.text}</Link></li>
             ))}
           </ul>
         </div>
-        <div className="col-xs-6 col-sm-3 end-xs">
+        <div className="col-xs-12 col-sm-3 center-xs end-sm">
           {languages.map(language => (
             <p
               className={'margin-clear language ' + (language === languageCode && 'active')}
@@ -38,14 +41,16 @@ Header.propTypes = {
   changeLanguage: PropTypes.func.isRequired,
   languageCode: PropTypes.string.isRequired,
   languages: PropTypes.arrayOf(PropTypes.string).isRequired,
-  content: PropTypes.object.isRequired
+  content: PropTypes.object.isRequired,
+  pathname: PropTypes.string.isRequired
 }
 
 
 const mapStateToProps = state => ({
   languageCode: state.content.languageCode,
   languages: state.content.languages,
-  content: state.content.siteContent
+  content: state.content.siteContent,
+  pathname: state.routing.locationBeforeTransitions.pathname
 });
 
 export default connect(mapStateToProps, { changeLanguage })(Header);
