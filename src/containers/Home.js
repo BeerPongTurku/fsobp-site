fff3f672d9ac98b8e9d6eb2c94df70285354f00c
niemisami <sami@nieminen.fi>
2019-07-19 08:32:58 +0300

    Add lappeenranta and separate collaborators and organizers

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -8,11 +8,13 @@ import { getYear } from '../helpers'
 import fsobpLogoBorder from '../public/images/fsobp_logo_border_500.png'
 import aaltoLogo from '../public/images/aalto_logo_500.png'
 import bptLogo from '../public/images/bpt_logo.png'
+import lbpLogo from '../public/images/lbp_logo.png'
 import mmPingisLogo from '../public/images/mm-pingis_logo.jpg'
 
-const organizerLogos = {
+const logos = {
   ABP: aaltoLogo,
   BPT: bptLogo,
+  LBP: lbpLogo,
   'MM-Pingis': mmPingisLogo
 }
 
@@ -27,7 +29,7 @@ const contentText = (paragraph, index, array) => (
 )
 
 const Home = ({ content }) => {
-  const { bodyText, organizers, organizersTitle } = content;
+  const { bodyText, organizers, organizersTitle, partnersTitle, partners } = content;
   return (
     <div className="page-wrapper text-center line-height-high" >
       <div className="row center-xs" >
@@ -35,28 +37,41 @@ const Home = ({ content }) => {
           <Link to="/"><img className="img img-responsive margin-top-2 margin-bottom-2" alt="FSOBP" src={fsobpLogoBorder} /></Link>
           <h1 className="text-center margin-1">Finnish Series of Beer Pong {getYear()}</h1>
           {bodyText.map(contentText)}
+
+          <div className="row center-xs margin-top-2 margin-bottom-2">
+            <div className="text-center margin-bottom-2">
+              <h3 className="title">{partnersTitle}</h3>
+              <div className="row center middle-xs">
+                {partners.map((partner, index) =>
+                  (
+                    <div
+                      key={partner.id}
+                      className={`col-xs-12`}>
+                      <a href={partner.link} target="_blank" rel="noopener noreferrer">
+                        <img style={{maxWidth: 350}} alt={partner.name} className="bp-images margin-1" src={logos[partner.name]} />
+                      </a>
+                    </div>
+                  ))
+                }
+              </div>
+            </div>
+          </div>
+
           <div className="row center-xs margin-top-2">
             <div className="text-center">
               <h3 className="title">{organizersTitle}</h3>
               <div className="row center middle-xs">
-                {organizers.map((organizer, index) => {
-                  let divMediumWidth;
-                  if(index === organizers.length - 1 && organizers.length % 2 !== 0) {
-                    divMediumWidth = 'col-sm-4';
-                  } else {
-                    divMediumWidth = 'col-sm-4';
-                  }
-                  return (
+                {organizers.map((organizer, index) =>
+                  (
                     <div
                       key={organizer.id}
-                      className={`col-xs-12 ${divMediumWidth}`}
+                      className={`col-xs-12 col-sm-4`}
                     >
                       <a href={organizer.link} target="_blank" rel="noopener noreferrer">
-                        <img alt={organizer.name} className="bp-images margin-1" src={organizerLogos[organizer.name]} />
+                        <img alt={organizer.name} className="bp-images margin-1" src={logos[organizer.name]} />
                       </a>
                     </div>
-                  )
-                })
+                  ))
                 }
               </div>
             </div>
